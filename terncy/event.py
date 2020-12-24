bfb81cf5c334320c3f18ebb8e20dd4ba8c9a1352
Ruixiong Wen <rx.wen218@gmail.com>
2020-12-24 19:14:08 +0800

    fix bad format

--- terncy/event.py
+++ terncy/event.py
@@ -6,17 +6,21 @@ class Connected:
     def __init__(self):
         pass
 
+
 class Disconnected:
     def __init__(self):
         pass
+
 
 class Discovered:
     def __init__():
         pass
 
+
 class Gone:
     def __init__():
         pass
+
 
 class EventMessage:
     def __init__(self, msg):
